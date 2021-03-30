05958923b6ddf5256c2b2aac445d8e557cb68b22
Omar Flores Grimontt <omar.flores@rappi.com>
2021-03-30 12:11:58 -0500

    feat(link): Added controlled error handling in createShortUrl.

--- packages/api-gw/src/adapters/link/link.js
+++ packages/api-gw/src/adapters/link/link.js
@@ -2,8 +2,19 @@ const { createError } = require('../../utils/errorHandling');
 
 async function createShortUrl(req, reply) {
   const { url } = req.body;
-  
+
   req.log.info(`Creating short url for ${url}`);
+
+  if (!url) {
+    return reply.code(400)
+      .headers('Content-Type', 'application/json; charset=utf-8')
+      .send(createError({
+        statusCode: 400,
+        error: 'Bad request',
+        message: 'URL field is required'
+      }));
+  }
+
   const hostnameUrl = `${req.protocol}://${req.hostname}`;
   const shortUrL = await this.linkAdapter.createShortUrl(url, hostnameUrl);
 
