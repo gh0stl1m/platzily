96fb5b694d52730469ffa2a512d0cb9435524447
Omar Flores Grimontt <omar.flores@rappi.com>
2021-03-29 19:23:54 -0500

    test(link): Added url redirect testing.

--- packages/api-gw/src/adapters/link.spec.js
+++ packages/api-gw/src/adapters/link.spec.js
@@ -1,26 +1,17 @@
 const Faker = require('faker');
 const linkAdapters = require('./link');
+const { createReqStub, createReplyStub } = require('../utils/testUtils');
 
 describe('Link Adapters' , () => {
   describe('createShortUrl' , () => {
     it('Given an url in the body, when a short link is created then the function must be return it', async () => {
       // Arrange
-      const reqStubs = {
-        protocol: Faker.internet.protocol(),
-        hostname: Faker.internet.domainName(),
-        log: {
-          info: jest.fn(),
-        }, 
+      const reqStubs = createReqStub({
         body: {
           url: Faker.internet.url(),
         }
-      };
-      const sendStub = jest.fn(() => ({ data: Faker.internet.url() }));
-      const headersStub = jest.fn(() => ({ send: sendStub }));
-      const codeReplystub = jest.fn(() => ({ headers: headersStub }))
-      const replyStubs = {
-        code: codeReplystub, 
-      };
+      });
+      const replyStubs = createReplyStub();
       const linkAdapterStub = {
         createShortUrl: jest.fn(),
       }
@@ -29,7 +20,7 @@ describe('Link Adapters' , () => {
       const createShortUrBinded = linkAdapters.createShortUrl
         .bind({ linkAdapter: linkAdapterStub });
       await createShortUrBinded(reqStubs, replyStubs);
-      
+
       // Asserts
       expect(reqStubs.log.info).toHaveBeenCalled();
       expect(replyStubs.code).toHaveBeenCalledWith(201);
@@ -37,4 +28,56 @@ describe('Link Adapters' , () => {
       expect(((replyStubs.code()).headers()).send).toHaveBeenCalled();
     });
   });
+
+  describe('redirectToUrl', () => {
+    it('Given a valid hash the function must redirect to the resulting url', async () => {
+      // Arrange
+      const reqStubs = createReqStub({
+        params: {
+          hash: Faker.random.uuid()
+        }
+      });
+      const replyStubs = createReplyStub();
+      const linkAdapterStub = {
+        readUrlByHash: jest.fn(() => Promise.resolve({ originalUrl: Faker.internet.url() })),
+      }
+
+      // Act
+      const readUrlByHashBinded = linkAdapters.redirectToUrl
+        .bind({ linkAdapter: linkAdapterStub });
+      await readUrlByHashBinded(reqStubs, replyStubs);
+
+       // Asserts
+       expect(reqStubs.log.info).toHaveBeenCalled();
+       expect((replyStubs.redirect)).toHaveBeenCalled();
+    })
+
+    it('Given an invalid hash the function must return a 404 error', async () => {
+       // Arrange
+       const reqStubs = createReqStub({
+        params: {
+          hash: Faker.random.uuid()
+        }
+      });
+      const replyStubs = createReplyStub();
+      const linkAdapterStub = {
+        readUrlByHash: jest.fn(() => Promise.resolve(null)),
+      }
+
+      // Act
+      const readUrlByHashBinded = linkAdapters.redirectToUrl
+        .bind({ linkAdapter: linkAdapterStub });
+      await readUrlByHashBinded(reqStubs, replyStubs);
+
+      // Asserts
+      expect(reqStubs.log.info).toHaveBeenCalled();
+      expect(replyStubs.code).toHaveBeenCalledWith(404);
+      expect((replyStubs.code()).headers).toHaveBeenCalled();
+      expect(((replyStubs.code()).headers()).send).toHaveBeenCalledWith({
+        statusCode: 404,
+        error: "Not found",
+        message: "Oops, it looks like this url do not exist anymore"
+      });
+    })
+  })
 });
